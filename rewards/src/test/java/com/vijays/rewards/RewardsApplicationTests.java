6e293557c272ae534ab17ff269d9f305c4b8409a
vijays <vijays@users.noreply.github.com>
2022-09-14 14:17:34 +0530

    Smoke test to check AppController is called and instantiated properly

--- rewards/src/test/java/com/vijays/rewards/RewardsApplicationTests.java
+++ rewards/src/test/java/com/vijays/rewards/RewardsApplicationTests.java
@@ -1,13 +1,24 @@
 package com.vijays.rewards;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.vijays.rewards.controllers.AppController;
+
+// Smoke test to check AppController is called and instantiated properly
 
 @SpringBootTest
 class RewardsApplicationTests {
 
+	@Autowired
+	private AppController controller;
+
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
 }
