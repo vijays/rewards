d6b5462ed3e57f36ddd7453e1a338e22d64f8791
vijays <vijays@users.noreply.github.com>
2022-09-14 13:36:36 +0530

    ApplicationRunner - calculate reward points and update transaction table

--- rewards/src/main/java/com/vijays/rewards/RewardsApplication.java
+++ rewards/src/main/java/com/vijays/rewards/RewardsApplication.java
@@ -1,13 +1,45 @@
 package com.vijays.rewards;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.vijays.rewards.entities.Transaction;
+import com.vijays.rewards.repositories.ITransactionRepo;
+
 @SpringBootApplication
-public class RewardsApplication {
+public class RewardsApplication implements ApplicationRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RewardsApplication.class, args);
+	}
+	
+	@Autowired
+	ITransactionRepo transactionRepo ;	
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		List<Transaction> transactionList = new ArrayList<Transaction>() ;
+
+		transactionList = transactionRepo.findAll() ;
+		for (Transaction transaction:transactionList) {
+
+			int rewardPoints = 0;
+			int transactionAmount = transaction.getAmount_transaction();
+			if (transactionAmount < 50) 
+				rewardPoints = transactionAmount ;
+			else
+				rewardPoints = 50 + (transactionAmount - 50)*2 ;
+			transaction.setReward_points_transaction(rewardPoints);
+			
+			transactionRepo.save(transaction) ;
+		}		
+		
 	}
 
 }
