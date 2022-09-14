c7b9391d2ebcfac3696d4298b2eae505b586c54a
vijays <vijays@users.noreply.github.com>
2022-09-14 11:47:53 +0530

    Interface projections to save results of queries

---
+++ rewards/src/main/java/com/vijays/rewards/entities/IpMonthlyRewards.java
@@ -0,0 +1,8 @@
+package com.vijays.rewards.entities;
+
+// Interface Projection to map result of SQL query
+public interface IpMonthlyRewards {
+
+	int getreward_month() ;
+	int getreward_points() ;
+}
---
+++ rewards/src/main/java/com/vijays/rewards/entities/IpTotalRewards.java
@@ -0,0 +1,6 @@
+package com.vijays.rewards.entities;
+
+//Interface Projection to map result of SQL query
+public interface IpTotalRewards {
+	int gettotal_reward_points() ;
+}
